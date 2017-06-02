519b4d1511c65bf43388d9ac8fafaea1b29624a4
Xian Jingxiong <xianjx@staff.tianya.cn>
2017-06-02 10:45:26 +0800

    添加三种状态按钮

--- app/src/main/java/com/shane/me/shanedemo/activity/CustomProgressBarActivity.java
+++ app/src/main/java/com/shane/me/shanedemo/activity/CustomProgressBarActivity.java
@@ -4,17 +4,34 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 import com.shane.me.shanedemo.R;
 import com.shane.me.shanedemo.widget.LiveRecordProgressBar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnLongClick;
+import butterknife.OnTouch;
 
 public class CustomProgressBarActivity extends BaseActivity {
 
+    private static final int STATE_NORMAL = 0;
+    private static final int STATE_PRESSED = 1;
+    private static final int STATE_RECORDING = 2;
+
     @BindView(R.id.progressbar_test)
     LiveRecordProgressBar progressBar;
+
+    @BindView(R.id.btn_press)
+    View pressBtn;
+
+    boolean isRecording;
+    boolean isLongClick;
+
 
     static final int MAX_COUNT = 30 * 1000;
     int count;
@@ -54,6 +71,43 @@ public class CustomProgressBarActivity extends BaseActivity {
         }, DELAY);
     }
 
+
+    @OnClick(R.id.btn_press)
+    void onBtnClick() {
+        isLongClick = false;
+    }
+
+    @OnLongClick(R.id.btn_press)
+    boolean onLongClick() {
+        isLongClick = true;
+        return true;
+    }
+
+    @OnTouch(R.id.btn_press)
+    boolean onTouch(View view, MotionEvent event) {
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                isLongClick = false;
+                setButtonState(STATE_PRESSED);
+
+                isRecording = !isRecording;
+                break;
+            case MotionEvent.ACTION_UP:
+                if (isLongClick) {
+                    setButtonState(STATE_NORMAL);
+                    if (isRecording) {
+                        isRecording = false;
+                    }
+                } else {
+                    setButtonState(isRecording ? STATE_RECORDING : STATE_NORMAL);
+                }
+                break;
+        }
+
+        return false;
+    }
+
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -65,6 +119,24 @@ public class CustomProgressBarActivity extends BaseActivity {
         super.onStop();
         progressBar.stopAutoDraw();
     }
+
+
+    void setButtonState(int state) {
+        switch (state) {
+            case STATE_NORMAL:
+                pressBtn.setBackgroundResource(R.drawable.button_record_normal);
+                break;
+            case STATE_PRESSED:
+                pressBtn.setBackgroundResource(R.drawable.button_record_pressed);
+                break;
+            case STATE_RECORDING:
+                pressBtn.setBackgroundResource(R.drawable.button_recording);
+                break;
+            default:
+                pressBtn.setBackgroundResource(R.drawable.button_record_normal);
+        }
+    }
+
 
 
 
