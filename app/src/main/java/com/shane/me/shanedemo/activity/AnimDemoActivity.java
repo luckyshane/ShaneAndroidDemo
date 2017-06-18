4d37520cab3dafb90ff0ad6e7c6d84c864e153e5
Shane <luckyshane@foxmail.com>
2017-06-18 18:21:07 +0800

    添加抖动动画

---
+++ app/src/main/java/com/shane/me/shanedemo/activity/AnimDemoActivity.java
@@ -0,0 +1,98 @@
+package com.shane.me.shanedemo.activity;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+
+import com.shane.me.shanedemo.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class AnimDemoActivity extends BaseActivity {
+
+    @BindView(R.id.btn_target)
+    Button targetBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anim_demo);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_translate)
+    void onTranslate() {
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(targetBtn, "translationY", 10, -200);
+        objectAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        objectAnimator.setDuration(300);
+        objectAnimator.start();
+    }
+
+    @OnClick(R.id.btn_scale)
+    void onScale() {
+        ObjectAnimator scaleAnimator = ObjectAnimator.ofFloat(targetBtn, "scaleX", 0, 1.2f, 1);
+        scaleAnimator.setDuration(3000);
+        scaleAnimator.start();
+    }
+
+    @OnClick(R.id.btn_alpha)
+    void onAlpha() {
+        ObjectAnimator alpahAnimator = ObjectAnimator.ofFloat(targetBtn, "alpha", 0, 1.0f);
+        alpahAnimator.setDuration(3000);
+        alpahAnimator.start();
+    }
+
+
+    @OnClick(R.id.btn_all)
+    void onAllAnim() {
+        AnimatorSet showAnimSet = new AnimatorSet();
+
+        ObjectAnimator translateAnimator = ObjectAnimator.ofFloat(targetBtn, "translationY", 0, -200);
+        //translateAnimator.setInterpolator(new AccelerateInterpolator());
+        translateAnimator.setDuration(1000);
+
+        ObjectAnimator scaleAnimator = ObjectAnimator.ofFloat(targetBtn, "scaleX", 0, 1);
+        scaleAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        scaleAnimator.setDuration(1000);
+
+        ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(targetBtn, "scaleY", 0, 1);
+        scaleYAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        scaleYAnimator.setDuration(1000);
+
+        ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(targetBtn, "alpha", 0, 1.0f);
+        //alphaAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        alphaAnimator.setDuration(500);
+
+        showAnimSet.playTogether(translateAnimator, scaleAnimator, scaleYAnimator, alphaAnimator);
+
+        ObjectAnimator shakeAnimator = ObjectAnimator.ofFloat(targetBtn, "translationY",
+                -200,
+                -220,
+                -180,
+                -210,
+                -190,
+                -200);
+        //shakeAnimator.setRepeatCount(1);
+        shakeAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        shakeAnimator.setDuration(1000);
+
+
+        AnimatorSet allSet = new AnimatorSet();
+        allSet.playSequentially(showAnimSet, shakeAnimator);
+        allSet.setInterpolator(new LinearInterpolator());
+
+        allSet.start();
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/shane/me/shanedemo/util/ToastUtil.java
@@ -0,0 +1,16 @@
+package com.shane.me.shanedemo.util;
+
+import android.content.Context;
+
+/**
+ * Created by Asus on 2017/6/18.
+ */
+
+public class ToastUtil {
+
+    public static void shortToast(Context context, String text) {
+
+    }
+
+
+}
