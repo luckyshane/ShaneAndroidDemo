f0cd5170fbda4dc196e51e3e143f1e05cf4c1df7
luckyshane <xiaoxuan.lucky@gmail.com>
2017-10-27 09:54:46 +0800

    添加阴影测试，http://inloop.github.io/shadow4android/生成阴影.9图

---
+++ app/src/main/java/com/shane/me/shanedemo/activity/ShadowDemoActivity.java
@@ -0,0 +1,25 @@
+package com.shane.me.shanedemo.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.shane.me.shanedemo.R;
+
+/**
+ * Created by luckyshane on 2017/10/27.
+ */
+
+public class ShadowDemoActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shadow_demo);
+    }
+
+
+
+
+
+
+}
