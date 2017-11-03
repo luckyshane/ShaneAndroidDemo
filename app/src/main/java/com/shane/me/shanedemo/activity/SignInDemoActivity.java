260cce6b357d452edacee7ef05444141ae137b6d
luckyshane <xiaoxuan.lucky@gmail.com>
2017-11-03 08:34:49 +0800

    添加注册

---
+++ app/src/main/java/com/shane/me/shanedemo/activity/SignInDemoActivity.java
@@ -0,0 +1,26 @@
+package com.shane.me.shanedemo.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.shane.me.shanedemo.R;
+
+/**
+ * Created by luckyshane on 2017/10/27.
+ */
+
+public class SignInDemoActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sigin_in);
+        //getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/shane/me/shanedemo/activity/TestActivity.java
@@ -0,0 +1,19 @@
+package com.shane.me.shanedemo.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.shane.me.shanedemo.R;
+
+/**
+ * Created by luckyshane on 2017/10/28.
+ */
+
+public class TestActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
