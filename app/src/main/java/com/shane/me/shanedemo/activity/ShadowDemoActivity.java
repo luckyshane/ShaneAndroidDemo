eba9506f9749d9ac9939d9b9e9b781aab28f800b
luckyshane <xiaoxuan.lucky@gmail.com>
2017-12-19 09:52:41 +0800

    添加shadow测试

--- app/src/main/java/com/shane/me/shanedemo/activity/ShadowDemoActivity.java
+++ app/src/main/java/com/shane/me/shanedemo/activity/ShadowDemoActivity.java
@@ -2,8 +2,14 @@ package com.shane.me.shanedemo.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
+import android.view.View;
 
 import com.shane.me.shanedemo.R;
+import com.shane.me.shanedemo.util.ContextUtil;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by luckyshane on 2017/10/27.
@@ -11,14 +17,18 @@ import com.shane.me.shanedemo.R;
 
 public class ShadowDemoActivity extends BaseActivity {
 
+    @BindView(R.id.shadow_btn)
+    View shadowBtn;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shadow_demo);
+        ButterKnife.bind(this);
+
+        ViewCompat.setElevation(shadowBtn, ContextUtil.dip2px(this, 4));
     }
-
-
-
 
 
 
