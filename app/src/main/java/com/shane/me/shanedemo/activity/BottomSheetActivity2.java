973e29e49b1b1767e4292248b3ab91705b55b056
luckyshane <xiaoxuan.lucky@gmail.com>
2018-02-08 14:42:37 +0800

    BottomSheet demo2

---
+++ app/src/main/java/com/shane/me/shanedemo/activity/BottomSheetActivity2.java
@@ -0,0 +1,8 @@
+package com.shane.me.shanedemo.activity;
+
+/**
+ * Created by luckyshane on 2018/2/8.
+ */
+
+public class BottomSheetActivity2 {
+}
---
+++ app/src/main/java/com/shane/me/shanedemo/widget/LockableBottomSheetBehavior.java
@@ -0,0 +1,76 @@
+package com.jacksen.supportlibrarydemo;
+
+import android.content.Context;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by luckyshane on 2018/2/7.
+ */
+
+public class LockableBottomSheetBehavior<V extends View> extends BottomSheetBehavior<V> {
+    private boolean isLocked = true;
+
+    public LockableBottomSheetBehavior() {
+        super();
+    }
+
+    public LockableBottomSheetBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setLocked(boolean isLocked) {
+        this.isLocked = isLocked;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(CoordinatorLayout parent, V child, MotionEvent event) {
+        if (!isLocked) {
+            return super.onInterceptTouchEvent(parent, child, event);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(CoordinatorLayout parent, V child, MotionEvent event) {
+        if (!isLocked) {
+            return super.onTouchEvent(parent, child, event);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, V child, View directTargetChild, View target, int nestedScrollAxes) {
+        if (!isLocked) {
+            return super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+        }
+        return false;
+    }
+
+    @Override
+    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, V child, View target, int dx, int dy, int[] consumed) {
+        if (!isLocked) {
+            super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed);
+        }
+    }
+
+    @Override
+    public void onStopNestedScroll(CoordinatorLayout coordinatorLayout, V child, View target) {
+        if (!isLocked) {
+            super.onStopNestedScroll(coordinatorLayout, child, target);
+        }
+    }
+
+    @Override
+    public boolean onNestedPreFling(CoordinatorLayout coordinatorLayout, V child, View target, float velocityX, float velocityY) {
+        if (!isLocked) {
+            return super.onNestedPreFling(coordinatorLayout, child, target, velocityX, velocityY);
+        }
+        return false;
+
+    }
+
+}
