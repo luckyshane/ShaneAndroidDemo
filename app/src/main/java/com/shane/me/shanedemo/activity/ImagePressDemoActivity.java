9221a9e2a62ab7e16b02a35105937e46fddb42a6
luckyshane <xiaoxuan.lucky@gmail.com>
2018-04-20 15:46:47 +0800

    图片按钮，增加点击背景为圆形增加一个半透明的蒙层。

---
+++ app/src/main/java/com/shane/me/shanedemo/activity/ImagePressDemoActivity.java
@@ -0,0 +1,22 @@
+package com.shane.me.shanedemo.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.shane.me.shanedemo.R;
+
+/**
+ * Created by luckyshane on 2018/4/20.
+ */
+
+public class ImagePressDemoActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_press_demo);
+    }
+
+
+
+}
